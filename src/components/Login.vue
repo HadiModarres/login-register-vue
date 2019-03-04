0c5dc863eaacfdcf2ff675ea3477bd3fc1b9782c
Hadi Modarres <modarres.zadeh@gmail.com>
2019-03-04 14:35:44 +1100

    login page basic

---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+     <h1>Login</h1>
+     <form>
+       <div class="form-group">
+         <label>Email Address</label>
+         <input class="form-control" type="text" placeholder="Enter your email address">
+
+       </div>
+       <div class="form-group">
+         <label>Password</label>
+         <input class="form-control" type="text" placeholder="Enter your password">
+       </div>
+       <div class="form-group">
+        <button class="btn btn-primary">
+         Login
+        </button>
+         <router-link to="/">Register</router-link>
+       </div>
+
+     </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
